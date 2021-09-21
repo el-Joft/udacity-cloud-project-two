c80bf0f74a69a8feb1f108903fc7ae49452255f8
el-joft <ottimothy@gmail.com>
2021-09-21 17:23:49 +0100

    fixing isValidURL regex build

--- www/util/util.js
+++ www/util/util.js
@@ -50,13 +50,8 @@ function deleteLocalFiles(files) {
 }
 exports.deleteLocalFiles = deleteLocalFiles;
 function validURL(str) {
-    var pattern = new RegExp("^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-        "(\\#[-a-z\\d_]*)?$", "i"); // fragment locator
-    return !!pattern.test(str);
+    const res = str.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return res !== null;
 }
 exports.validURL = validURL;
 //# sourceMappingURL=util.js.map
